66e247f13b05892ec2872abf3feab7a5b594dacb
IreneValor <115938535+IreneValor@users.noreply.github.com>
2023-04-19 20:16:21 +0200

    done

--- routes/products.js
+++ routes/products.js
@@ -36,6 +36,7 @@ router.get("/:id/edit", async (req, res, next) => {
 });
 
 router.post("/:id/edit", async (req, res, next) => {
+  const { id } = req.params;
   await Product.findByIdAndUpdate(id, req.body);
   res.redirect("/products");
 });
